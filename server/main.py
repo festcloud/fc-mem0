cca4665e8ef8a3509212faf040d2f636b70cb914
anastasiiapeleshok <anastasiia.peleshok@festcloud.ai>
2026-02-13 19:07:38 +0200

    Reapply "MP-239: changed parameters for neo4j"
    This reverts commit 4ff478c38044c4b80686e86fbf270f05d1c866dd.

--- server/main.py
+++ server/main.py
@@ -172,7 +172,9 @@ DEFAULT_CONFIG = {
     },
     "graph_store": {
         "provider": "neo4j",
-        "config": {"url": NEO4J_URI, "username": NEO4J_USERNAME, "password": NEO4J_PASSWORD, "database": "neo4j"},
+        "url": NEO4J_URI,
+        "username": NEO4J_USERNAME,
+        "password": NEO4J_PASSWORD
     },
     "llm": {"provider": "gemini", "config": {"api_key": GOOGLEAI_API_KEY, "temperature": 0.2, "model": "gemini-2.5-flash", "max_tokens": 124000}},
     "embedder": {"provider": "gemini", "config": {"api_key": GOOGLEAI_API_KEY, "model": "gemini-embedding-001", "embedding_dims": 1536}},
