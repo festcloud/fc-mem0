8f0de64ead7dfd9f4d33963dd04865d4d830eabf
anastasiiapeleshok <anastasiia.peleshok@festcloud.ai>
2026-02-16 14:41:37 +0200

    MP-262. Removed default elastic port

--- server/main.py
+++ server/main.py
@@ -191,7 +191,7 @@ BASE_CONFIG = {
         "config": {
             "collection_name": ELASTICSEARCH_COLLECTION_NAME,
             "host": ELASTICSEARCH_URI,
-            "port": int(ELASTICSEARCH_PORT or 9200),
+            "port": int(ELASTICSEARCH_PORT),
             "auto_create_index": True,
             "user": ELASTICSEARCH_USER,
             "password": ELASTICSEARCH_PASSWORD,
