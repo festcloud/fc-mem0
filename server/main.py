d9fb332129d3d6ae7ff04df1952e975d2be92c85
anastasiiapeleshok <anastasiia.peleshok@festcloud.ai>
2026-02-18 17:03:58 +0200

    MP-270. Fixed graph store for large data processing

--- server/main.py
+++ server/main.py
@@ -221,7 +221,7 @@ BASE_CONFIG = {
         "provider": "gemini",
         "config": {
             "api_key": GOOGLEAI_API_KEY,
-            "temperature": 0.2,
+            "temperature": 0.1,
             "model": "gemini-2.5-flash",
             "max_tokens": 700000
         }
@@ -241,9 +241,11 @@ USER_INFO_CONFIG["custom_fact_extraction_prompt"] = FACT_EXTRACTION_PROMPT_USER_
 
 KNOWLEDGE_BASE_CONFIG = copy.deepcopy(BASE_CONFIG)
 KNOWLEDGE_BASE_CONFIG["custom_fact_extraction_prompt"] = FACT_EXTRACTION_PROMPT_SYSTEM_KNOWLEDGE
+KNOWLEDGE_BASE_CONFIG["llm"]["config"]["model"] = "gemini-3-flash-preview"
 
 ARTIFACT_BASE_CONFIG = copy.deepcopy(BASE_CONFIG)
 ARTIFACT_BASE_CONFIG["custom_fact_extraction_prompt"] = FACT_EXTRACTION_PROMPT_ARTIFACT_KNOWLEDGE
+ARTIFACT_BASE_CONFIG["llm"]["config"]["model"] = "gemini-3-flash-preview"
 
 MEMORY_INSTANCES: Dict[str, Memory] = {}
 
